86abf9d365a3197f99b2eaf4644fcae621a1d875
florianingerl <imelflorianingerl@gmail.com>
2023-06-04 05:05:29 +0200

    Adds vertical side navigation bar

---
+++ frontend/src/MyHome.vue
@@ -0,0 +1,31 @@
+<template>
+  <NavBar>
+    <h1>This is the content inside the navbar</h1>
+
+    <HelloUsers />
+  </NavBar>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+import HelloUsers from './components/HelloUsers.vue'
+
+export default {
+  name: 'MyHome',
+  components: {
+    NavBar,
+    HelloUsers
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import MyHome from './MyHome.vue'
 
-createApp(App).mount('#app')
+createApp(MyHome).mount('#app')
