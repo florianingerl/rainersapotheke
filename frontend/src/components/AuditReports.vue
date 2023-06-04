9ad47fb30b4fec0d2e291ec18624cc3dfd71101c
florianingerl <imelflorianingerl@gmail.com>
2023-06-04 20:27:20 +0200

    Correctly includes the bootstrap library

--- frontend/src/components/AuditReports.vue
+++ frontend/src/components/AuditReports.vue
@@ -3,15 +3,16 @@
 
 <button type="button" class="btn btn-primary" @click="this.newReportMode=true">Neuen Auditbericht erstellen</button>
 
-<!-- <NewReport v-if="newReportMode" /> -->
+<NewReport v-if="newReportMode" />
 
 </template>
 
 <script>
-
+import NewReport from './NewReport.vue';
 
 export default {
     name : 'AuditReports',
+    components : { NewReport },
     data(){
         return {
             newReportMode : false
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import MyHome from './MyHome.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 createApp(MyHome).mount('#app')
